da9078aa8095c34577555f178da861be6c2b5980
Sjoerd <sjoerdboonstra_626@hotmail.com>
2019-10-23 13:53:13 +0200

    pyyyy

--- app.py
+++ app.py
@@ -21,11 +21,17 @@ def get_hit_count():
             retries -= 1
             time.sleep(0.5)
 
-@app.route("/get_my_ip", methods=["GET"])
-def get_my_ip():
-    return jsonify({'ip': request.remote_addr}), 200
-    ip1 = (request.environ.get('HTTP_X_REAL_IP', request.remote_addr))
-    print ("IP:", ip1)
+import urllib
+import re
+
+def get_external_ip():
+    site = urllib.urlopen("http://checkip.dyndns.org/").read()
+    grab = re.findall('([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', site)
+    address = grab[0]
+    return address
+
+if __name__ == '__main__':
+  print( get_external_ip() )
 
 @app.route('/')
 def hello():
