e5fd4a7c33e5397fd3dea5f7c16d699ba610c313
Sjoerd <sjoerdboonstra_626@hotmail.com>
2019-10-23 13:57:39 +0200

    letsgo

--- app.py
+++ app.py
@@ -9,18 +9,6 @@ import socket
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
 import urllib
 import re
 
@@ -32,6 +20,17 @@ def get_external_ip():
 
 if __name__ == '__main__':
   print( get_external_ip() )
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 @app.route('/')
 def hello():
