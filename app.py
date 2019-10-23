d00c3589924135d4bfc24712a4bb3ec4274f95c9
Sjoerd <sjoerdboonstra_626@hotmail.com>
2019-10-23 14:27:42 +0200

    py update

--- app.py
+++ app.py
@@ -9,18 +9,6 @@ import socket
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
-import urllib
-import re
-
-def get_external_ip():
-    site = urllib.urlopen("http://checkip.dyndns.org/").read()
-    grab = re.findall('([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)', site)
-    address = grab[0]
-    return address
-
-if __name__ == '__main__':
-  print( get_external_ip() )
-
 def get_hit_count():
     retries = 5
     while True:
@@ -36,3 +24,6 @@ def get_hit_count():
 def hello():
     count = get_hit_count()
     return 'Hello World. Sjoerd & Jako here ! this app has been seen {} times.\n'.format(count)
+
+def get_my_ip():
+    return jsonify({'ip': request.remote_addr}), 200
